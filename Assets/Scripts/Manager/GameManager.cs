17e2d61349f5924564f1c7a4e090084355726b9d
tive1021 <tive1021@naver.com>
2024-11-09 23:48:50 +0900

    첫 페이지 임시 추가
    버튼과 돈 연결 완료
    파티클 연구 필요
    버튼 클릭시 마우스 포지션에 파티클 뜨도록 할 예정

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> 
+{
+    public BigInteger money = 0;
+    public TextMeshProUGUI moneyValueLabel;
+    ParticleSystem _particleSystem;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _particleSystem = GameObject.FindGameObjectWithTag("Particle").GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        moneyValueLabel.text = money.ToString();
+    }
+
+    public void OnClick()
+    {
+        money++;
+        _particleSystem.Play();
+    }
+}
